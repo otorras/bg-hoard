4123f844e98751c888f3dc8e4b09be11dd4cae1c
oriol <oriol@backbase.com>
2021-12-13 20:43:41 +0100

    Lab 8

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
@@ -24,6 +25,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
     ),
     MatCardModule,
     StoreUiSharedModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
