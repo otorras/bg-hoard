425b2988eb91db36dc5b0c6356ceb03f5dec0f30
oriol <oriol@backbase.com>
2021-12-13 18:56:32 +0100

    lab 4

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
@@ -12,6 +13,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
     MatCardModule,
+    StoreUiSharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
