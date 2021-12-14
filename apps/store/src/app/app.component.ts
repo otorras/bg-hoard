5f0bc3ce719041942715a2ac15ab65a1089a8aee
oriol <oriol@backbase.com>
2021-12-14 21:11:49 +0100

    Lab 20

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { Game } from '@bg-hoard/util-interface';
+import { getAllGames } from '../fake-api';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -10,7 +11,8 @@ import { Game } from '@bg-hoard/util-interface';
 })
 export class AppComponent {
   title = 'Board Game Hoard';
-  games = this.httpClient.get<Game[]>('/api/games');
+  games$ = this.httpClient.get<Game[]>('/api/games');
+  games = getAllGames();
   formatRating = formatRating;
 
   constructor(private readonly httpClient: HttpClient) {
